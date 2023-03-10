3caf5250e45882e51243f4060111e2e90e3b75ba
Fēlikss <103173146+21DP3FRegz@users.noreply.github.com>
2023-03-10 13:28:03 +0200

    Update transaction.py

--- transaction.py
+++ transaction.py
@@ -1,2 +1,10 @@
+import datetime
+
 class Transaction:
-    ...
+    def __init__(self, amount: float) -> None:
+        self.id = self.__create_id()
+        self.date = datetime.date.today()
+        self.amount = amount
+    
+    def __create_id() -> int:
+        return 123
