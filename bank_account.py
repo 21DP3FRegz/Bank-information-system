4222743f9292b23757b90670856a3b9dd9d82717
Fēlikss <103173146+21DP3FRegz@users.noreply.github.com>
2023-03-10 13:07:41 +0200

    added init to the Client

--- bank_account.py
+++ bank_account.py
@@ -1,2 +1,10 @@
-class account:
-    ...
+class Client:
+    def __init__(self, login: str, password: str, personal_id: int, name: str) -> None:
+        self.login = login
+        self.password = password
+
+        self.personal_id = personal_id
+        self.name = name
+        
+        self.phone = None
+        self.email = None
