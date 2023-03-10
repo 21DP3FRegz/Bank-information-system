661d2c8054b50620a1860175fd75b1deb474758e
Fēlikss <103173146+21DP3FRegz@users.noreply.github.com>
2023-03-10 13:09:33 +0200

    Update company.py

--- company.py
+++ company.py
@@ -1,2 +1,17 @@
-class company:
-    ...
+import datetime
+
+class Account:
+    def __init__(self, name: str, holder: str, account_type: str) -> None:
+        self.id = self.__create_id()
+        self.date_opened = datetime.date.today()
+
+        self.holder = holder
+        self.name = name
+        self.type = account_type
+        self.details = None
+    
+    def __create_id() -> int:
+        return 123
+
+    def add_details() -> None:
+        ...
