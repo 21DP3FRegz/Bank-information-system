67a10ca11ef102ec99f07c9828909cf75c1e5b43
Fēlikss <felikss.regzdins@gmail.com>
2023-03-12 21:40:26 +0200

    basic registration

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
+import sys
 from client import Client
 
 
 def register_user():
-    ...
+    name = input("Enter your Name : ")
+    print("10q! Now to continue registration Create a login:")
+    login = input()
+    # -_-
+    password = input("Create password : ")
+    # password != input("Confirm password : ") -> ...
+    print("U r successfully finished registration!!!")
+
+    new_client = Client(
+        name=name,
+        login=login,
+        password=password
+    )
+
+    # phone or email?
+
+    new_client.save()
 
 
 def main():
+    register_user()
+
     clients = Client.get_clients()
     print(*(client.__dict__.values() for client in clients), sep="\n")
 
