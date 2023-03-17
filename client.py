78de2f65d1e3c840f208a4dfd374ce01c2d0065c
Fēlikss <103173146+21DP3FRegz@users.noreply.github.com>
2023-03-17 12:31:46 +0200

    delete account

--- client.py
+++ client.py
@@ -1,6 +1,11 @@
+import os
+
 from account import Account
+from colors import Colors
+
 
 FILE = "clients.txt"
+clear = lambda: os.system('cls') if os.name == "nt" else os.system('clear')
 
 
 class Client:
@@ -15,6 +20,7 @@ class Client:
             file.write(":".join(str(value) for value in self.__dict__.values()) + "\n") # "login:password:name:age\n" foreach client
 
     def create_account(self) -> Account:
+        clear()
         print("\n======== Create Account ==========\n")
         new_account = Account(
             holder=self.login,
@@ -24,13 +30,26 @@ class Client:
         return new_account
     
     def delete_account(self):
-        return
-        with open(FILE, "r") as f:
-            lines = f.readlines()
-        with open(FILE, "w") as f:
-            for line in lines:
-                if line.strip("\n") != "nickname_to_delete":
-                    f.write(line)
+        clear()
+        print("\n======== Delete Account ==========\n")
+        accounts = self.get_accounts()
+        [print("[" + Colors.BLUE, i+1, Colors.END + "]", account.name) for i, account in enumerate(accounts)]
+        print("[" + Colors.BLUE + " B " + Colors.END + "] Back")
+        answer: str = input("\nWhich one account You want to delete : ").strip().lower()
+
+        if answer == 'b':
+            return
+        try:
+            answer: int = int(answer)
+        except:
+            return self.delete_account()
+        
+        if not 0 < answer <= len(accounts):
+            return self.delete_account()
+
+        account_to_delete: Account = accounts[answer-1]
+        id_to_delete = account_to_delete.id
+        Account.delete_accont_by_id(id_to_delete)
     
     def get_accounts(self) -> list:
         accounts = list(filter(lambda account: self.__is_mine(account), Account.get_accounts()))
