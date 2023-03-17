f1bcc49022ae625c1d77a70b54896ba2efbe2ff3
Fēlikss <103173146+21DP3FRegz@users.noreply.github.com>
2023-03-17 12:32:25 +0200

    delete account

--- account.py
+++ account.py
@@ -24,11 +24,22 @@ class Account:
             file.write(":".join(str(value) for value in self.__dict__.values()) + "\n")
 
     @staticmethod
+    def delete_accont_by_id(id: str):
+        with open(FILE, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+
+        with open(FILE, "w", encoding="utf-8") as f:
+            for line in lines:
+                account_id = str(line.split(':')[0])
+                if account_id != id:
+                    f.write(line)
+
+    @staticmethod
     def get_accounts() -> list:
         with open(FILE, 'r', encoding="utf-8") as file:
             accounts = list()
             for line in file.readlines():
-                id, holder, balance, date_opened, name = line.replace('\n', '').split(':')
+                id, holder, balance, date_opened, name = line.strip('\n').split(':')
                 date_opened = datetime.datetime.strptime(date_opened.replace('-', ''), "%Y%m%d").strftime("%Y-%m-%d")
                 accounts.append(Account(holder, name, id, date_opened, int(balance)))
             return accounts
